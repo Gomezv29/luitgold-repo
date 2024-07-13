03a204cc72b158ce7ed67a931e1f643b8807f136
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-07-13 02:06:06 +0000

    module1 data types

---
+++ module1-data-types.py
@@ -0,0 +1,11 @@
+greeting = 'Hello, friend!' 
+# below is intergers
+age = 35
+# below is examples of floats
+speed1 = 4.5
+speed2 = 4.0
+speed3 = 4.
+speed4 = 4
+# below is an example of a boolean
+am_i_ugly = False
+
