161cc14dfc2fca01994b951de60ab41467f9787f
Vanessa Gomez <vanessa.gomez2989@gmail.com>
2024-07-14 01:57:23 +0000

    about operators

---
+++ module1-operators.py
@@ -0,0 +1,31 @@
+# Operators in Python
+
+# Addition
+x = 5
+y = 3
+print(x + y)
+
+# Subtraction
+h = 4
+print(x - h)
+
+# Multiplication
+print(y * x)
+
+# Standard Division
+e = 6
+r = 2
+print(e / r)
+w = 7
+print(w / r)
+
+# Interger division
+print(e // r)
+print(w // r)
+
+# Modulus division
+print(e % r)
+print(w % r)
+
+# Power operator
+print(r ** y)
